b8083c4a8c561c2264c11c7eb3c9cd347c6a55e3
Xigwa <bonya377@gmail.com>
2024-08-29 21:14:21 +0200

    0.0.2version

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -3,10 +3,16 @@ package org.example;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Bot extends TelegramLongPollingBot {
+
+    private String selectedLanguage = "en";
 
     @Override
     public String getBotUsername() {
@@ -20,22 +26,43 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        long idFromUser = update.getMessage().getFrom().getId();
-        String textUser = update.getMessage().getText();
-        if (textUser.equals("/start")) {
-            sendText(idFromUser, "Hello I'm a Tg Math bot. \n I can add, subtract, multiply, divide. (+,-,*,/) \n Example of what you can send: \"12 + 12\".");
-        } else {
-            processUserMessage(idFromUser, textUser);
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            long idFromUser = update.getMessage().getFrom().getId();
+            String textUser = update.getMessage().getText();
+            if (textUser.equals("/start")) {
+                sendLanguageSelectionButton(idFromUser, "Choose language.");
+            } else {
+                processUserMessage(idFromUser, textUser);
+            }
+        } else if (update.hasCallbackQuery()) {
+            String callbackData = update.getCallbackQuery().getData();
+            long chatId = update.getCallbackQuery().getMessage().getChatId();
+            switch (callbackData) {
+                case "ukrainian":
+                    selectedLanguage = "uk";
+                    sendTextWithHelpButton(chatId, "Привіт! Я бот, який може додавати, віднімати, множити та ділити числа. (+,-,*,/)\nПриклад повідомлення: \"12 + 12\".");
+                    break;
+                case "english":
+                    selectedLanguage = "en";
+                    sendTextWithHelpButton(chatId, "Hello! I'm a bot that can add, subtract, multiply, and divide numbers. (+,-,*,/)\nExample of a message: \"12 + 12\".");
+                    break;
+                case "polish":
+                    selectedLanguage = "pl";
+                    sendTextWithHelpButton(chatId, "Cześć! Jestem botem, który może dodawać, odejmować, mnożyć i dzielić liczby. (+,-,*,/)\nPrzykład wiadomości: \"12 + 12\".");
+                    break;
+                case "help":
+                    sendHelpMessage(chatId);
+                    break;
+            }
         }
     }
 
     public void processUserMessage(long chatId, String message) {
         String[] cuted = parseInput(message);
         if (cuted == null) {
-            sendText(chatId, "Invalid format. Use: number1 operator number2 (12 + 12)");
+            sendTextWithHelpButton(chatId, getLocalizedMessage("invalid_format"));
             return;
         }
-
         double num1 = Double.parseDouble(cuted[0]);
         String operator = cuted[1];
         double num2 = Double.parseDouble(cuted[2]);
@@ -43,7 +70,6 @@ public class Bot extends TelegramLongPollingBot {
         String result = calculate(num1, operator, num2);
         sendText(chatId, result);
     }
-
     public String[] parseInput(String input) {
         input = input.replaceAll("\\s+", "");
 
@@ -56,6 +82,7 @@ public class Bot extends TelegramLongPollingBot {
             }
         return null;
     }
+
     public String calculate(double num1, String operator, double num2) {
         double result;
         switch (operator) {
@@ -70,21 +97,143 @@ public class Bot extends TelegramLongPollingBot {
                 break;
             case "/":
                 if (num2 == 0)
-                    return "Cannot divide by zero.";
+                    return getLocalizedMessage("divide_by_zero");
                 result = num1 / num2;
                 break;
             default:
-                return "Invalid operator. Use +, -, *, /.";
+                return getLocalizedMessage("invalid_operator");
         }
-        return "Result: " + result;
+        return getLocalizedMessage("result") + ": " + result;
     }
 
-    public void sendText (Long who, String what){
+    public void sendText(Long who, String what) {
         SendMessage send = SendMessage.builder().chatId(who.toString()).text(what).build();
         try {
             execute(send);
         } catch (TelegramApiException e) {
             throw new RuntimeException();
+        }
+    }
+
+    public void sendLanguageSelectionButton(Long chatId, String text) {
+        SendMessage message = SendMessage.builder().chatId(chatId.toString()).text(text).build();
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+
+        List<InlineKeyboardButton> row1 = new ArrayList<>();
+        InlineKeyboardButton ukrButton = InlineKeyboardButton.builder()
+                .text("Українська")
+                .callbackData("ukrainian")
+                .build();
+        InlineKeyboardButton engButton = InlineKeyboardButton.builder()
+                .text("English")
+                .callbackData("english")
+                .build();
+        InlineKeyboardButton polButton = InlineKeyboardButton.builder()
+                .text("Polski")
+                .callbackData("polish")
+                .build();
+        row1.add(ukrButton);
+        row1.add(engButton);
+        row1.add(polButton);
+        rows.add(row1);
+
+        markup.setKeyboard(rows);
+        message.setReplyMarkup(markup);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendTextWithHelpButton(Long chatId, String text) {
+        SendMessage message = SendMessage.builder().chatId(chatId.toString()).text(text).build();
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rows = new ArrayList<>();
+
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        InlineKeyboardButton helpButton = InlineKeyboardButton.builder()
+                .text(getLocalizedMessage("help_button"))
+                .callbackData("help")
+                .build();
+        row.add(helpButton);
+        rows.add(row);
+
+        markup.setKeyboard(rows);
+        message.setReplyMarkup(markup);
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendHelpMessage(Long chatId) {
+        String helpMessage;
+        switch (selectedLanguage) {
+            case "uk":
+                helpMessage = "Щоб скористатися ботом, відправте повідомлення у форматі:\n\n<number1> <operator> <number2>\n\nНаприклад:\n12 + 12\n15 * 4\n7.5 / 2.5";
+                break;
+            case "pl":
+                helpMessage = "Aby skorzystać z bota, wyślij wiadomość w następującym formacie:\n\n<number1> <operator> <number2>\n\nNa przykład:\n12 + 12\n15 * 4\n7.5 / 2.5";
+                break;
+            default:
+                helpMessage = "To use this bot, send a message in the following format:\n\n<number1> <operator> <number2>\n\nFor example:\n12 + 12\n15 * 4\n7.5 / 2.5";
+                break;
+        }
+        sendText(chatId, helpMessage);
+    }
+    public String getLocalizedMessage(String key) {
+        switch (key) {
+            case "invalid_format":
+                switch (selectedLanguage) {
+                    case "uk":
+                        return "Невірний формат.";
+                    case "pl":
+                        return "Nieprawidłowy format.";
+                    default:
+                        return "Invalid format.";
+                }
+            case "divide_by_zero":
+                switch (selectedLanguage) {
+                    case "uk":
+                        return "Неможливо ділити на нуль.";
+                    case "pl":
+                        return "Nie można dzielić przez zero.";
+                    default:
+                        return "Cannot divide by zero.";
+                }
+            case "invalid_operator":
+                switch (selectedLanguage) {
+                    case "uk":
+                        return "Неправильний оператор. Використовуйте +, -, *, /.";
+                    case "pl":
+                        return "Nieprawidłowy operator. Użyj +, -, *, /.";
+                    default:
+                        return "Invalid operator. Use +, -, *, /.";
+                }
+            case "result":
+                switch (selectedLanguage) {
+                    case "uk":
+                        return "Результат";
+                    case "pl":
+                        return "Wynik";
+                    default:
+                        return "Result";
+                }
+            case "help_button":
+                switch (selectedLanguage) {
+                    case "uk":
+                        return "Допомога";
+                    case "pl":
+                        return "Pomoc";
+                    default:
+                        return "Help";
+                }
+            default:
+                return "";
         }
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,11 +8,9 @@ public class Main {
     private static String id = "7296799246";
     private static String token = "AAFGmvnGFMFbPdMh-vbXN75Sti-wmI_Z2k4";
     public static void main(String[] args) {
-        // Создаем объект TelegramBotsApi
         TelegramBotsApi botsApi = null;
         try {
             botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            // Регистрируем нашего бота
             botsApi.registerBot(new Bot());
             System.out.println("Бот успешно запущен!");
         } catch (TelegramApiException e) {
